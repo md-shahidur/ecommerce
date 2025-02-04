fa2e34d09b3e7d9e418b5c9e52b6860fc70db572
rajib1121 <rajib1121@gmail.com>
2025-02-05 00:11:20 +0600

    update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include(front.urls)),
     path('admin/', admin.site.urls),
+    path('', include("front.urls")),
 
 ]
