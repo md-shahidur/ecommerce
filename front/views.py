e611123b4b90fb49afa9459a12b94385648c0b87
rajib1121 <rajib1121@gmail.com>
2025-02-11 00:43:14 +0600

    item update

--- front/views.py
+++ front/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Item, Category
 
 # Create your views here.
 
 
 def index(request):
+    all_items = Item.objects.all()
+    for item in all_items:
+        print(item.price)
     return render(request, 'front/index.html', {
+        'items': all_items
 
     })
