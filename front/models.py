dd2cb922cec2647dfd0cdeb20438d92987684571
rajib1121 <rajib1121@gmail.com>
2025-02-10 23:30:46 +0600

    Update

--- front/models.py
+++ front/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='img/category/')
+    description = models.CharField(max_length=100, null=True, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=20, null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.CharField(max_length=150, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='img/item/')
+    category = models.ForeignKey(Category, null=True, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        return str(self.name)
--- members/models.py
+++ members/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
@@ -12,11 +12,17 @@ class Profile(models.Model):
     gender = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(
         max_length=50, null=True, blank=True, unique=True)
-    mobile = models.IntegerField(
-        null=True, blank=True, unique=True)
+    mobile = models.CharField(
+        max_length=11,
+        validators=[RegexValidator(
+            # Example regex for international phone numbers
+            regex=r'^\+?1?\d{9,15}$',
+            message="Phone number must be entered in the format: '01234567890'. Up to 11 digits allowed."
+        )]
+    )
     address = models.CharField(max_length=50, null=True, blank=True)
 
-    created_on = models.DateTimeField(default=datetime.datetime.now())
+    created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
     pro_pic = models.ImageField(
