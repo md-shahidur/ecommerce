a956692c8b872f96fd1754e69bcb55a2532b55b4
rajib1121 <rajib1121@gmail.com>
2025-02-18 21:13:06 +0600

    cart update

--- front/views.py
+++ front/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
+
 from django.contrib.auth.decorators import login_required
 from .models import Item, Category
+from cart.models import CartItem
 
 # Create your views here.
 
 
 def index(request):
     all_items = Item.objects.all()
+    user = request.user
+    if user:
+        all_cart_items = CartItem.objects.filter(user=user.id)
+        cart_item_count = len(all_cart_items)
+        print(f'Cart Items:{cart_item_count}')
     for item in all_items:
         print(item.price)
         print(item.category, type(item.category))
     return render(request, 'front/index.html', {
         'items': all_items
-
     })
 
 
