58063e0776abf5135f1a517fbaded17df44003b5
rajib1121 <rajib1121@gmail.com>
2025-02-24 23:16:25 +0600

    cart update

--- order/views.py
+++ order/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 
 def order_summery(request):
-    return HttpResponse('Page available')
+    if request.method == "POST":
+        # ids = request.POST['ids']
+        qty = json.load(qty)
+        print(qty)
+        msg = {'result': 'Ajax Success'}
+        return JsonResponse(msg)
